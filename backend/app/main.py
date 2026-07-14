162df2f401edb6934af2a662c5bf68d1d274d438
Anwesha Nayak <anweshanayak646@gmail.com>
2026-07-14 15:32:26 +0530

    Allow frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -132,12 +132,14 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://stocksphere-one.vercel.app",
+    ],
     allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 
 # ---------------------------------------------------------------------------
 # Routers
